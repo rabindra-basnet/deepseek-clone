bdd66b43503feee64729a6028adcd6e11a49ee01
rabindra-basnet <120715649+rabindra-basnet@users.noreply.github.com>
2025-05-02 08:23:22 +0000

    added the config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+        serverActions: {
+          allowInvalidOrigin: true,
+        },
+      },
+    async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://stunning-waddle-pvpx4wpwvqv26xwp-3000.app.github.dev/:path*',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
